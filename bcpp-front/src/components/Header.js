3108c0182b080b7f92a884ad8cf171cbbfc13305
Dolade <59974201+Dolade@users.noreply.github.com>
2022-02-05 21:38:18 -0800

    Some frontend work

--- bcpp-front/src/components/Header.js
+++ bcpp-front/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
 	render() {
 		return (
-			<div id="header">
+			<div id="header" class="center">
 				<h1>BC Part Picker</h1>
 			</div>
 		);
