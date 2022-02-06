bec49c02bad55269cea227c4b5c2fcb5dda293a1
Liam Harbec <liam.harbec@gmail.com>
2022-02-06 15:57:17 +0000

    Added half-finished code for the final endpoint:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :parts
-  resources :categories
+  resources :gears
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/categoriesearch/:name', to: 'categories#search'
+  get 'categoriesearch/:name', to: 'categories#search'
+  get 'compare/:id', to: 'parts#compare'
+  get 'build/:name', to: 'categories#build'
 end
