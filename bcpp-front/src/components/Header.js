e5ad71008cc69d8a5e42885c4685a98a54f6da94
Joels17 <joel.semeniuk@gmail.com>
2022-02-05 21:39:42 -0800

    Start picking

--- bcpp-front/src/components/Header.js
+++ bcpp-front/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Header extends React.Component {
 	render() {
 		return (
-			<div id="header" class="center">
+			<div id="header" className="center">
 				<h1>BC Part Picker</h1>
 			</div>
 		);
--- bcpp-front/src/components/SearchCategory.js
+++ bcpp-front/src/components/SearchCategory.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 export default class SearchCategory extends React.Component {
+	constructor() {
+		super();
+		this.state = { searchValue: '' };
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({ searchValue: e.target.value });
+	}
+
 	render() {
 		return (
 			<div id="searchcategory">
-				<form>
-					<input type="text" placeholder="Search.." />
+				<form method="get" id="searchcategoryForm">
+					<input onChange={this.onChange} type="text" placeholder="Search.." />
 				</form>
+				<button
+					disabled={!this.state.searchValue}
+					type="submit"
+					form="searchcategoryForm"
+					value="Submit"
+				>
+					Start Picking!
+				</button>
 			</div>
 		);
 	}
