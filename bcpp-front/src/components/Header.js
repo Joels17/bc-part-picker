9c355b2a3a6299aaa463935d5a4e9a5c6a1db086
Joels17 <joel.semeniuk@gmail.com>
2022-02-05 20:28:04 -0800

    Structured react

---
+++ bcpp-front/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+	render() {
+		return (
+			<div id="header">
+				<h1>BC Part Picker</h1>
+			</div>
+		);
+	}
+}
